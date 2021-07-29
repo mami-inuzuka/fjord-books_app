cafab7429b2dde80710ba1e2be37d0b81c80a645
mami-inuzuka <inuzuka.mami@gmail.com>
2021-07-29 21:37:01 +0900

    ユーザー一覧ページの作成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  # GET /users
+  def index
+    @users = User.all.order(:id)
+  end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   get 'mypage/show'
   root to: 'books#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  devise_scope :user do
+    get 'users' => 'users/registrations#index'
+  end
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
