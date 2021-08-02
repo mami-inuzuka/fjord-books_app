96b6632fb553e4034d964f27f855cc0c73a39e31
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-02 14:05:27 +0900

    新しく追加したカラムの値を保存できるようにストロングパラメータの設定をする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :zipcode, :address, :bio])
+  end
+
 end
