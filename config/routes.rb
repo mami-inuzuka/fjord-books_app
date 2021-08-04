7969707adcb6c5d6d993c4f80b10b761fff5b316
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-04 17:04:39 +0900

    デフォルトの/users/editを使えないように/booksにリダイレクトさせる

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'books#index'
   get 'mypage' => 'mypage#show'
+  get 'users/edit' => redirect('books')
   devise_for :users, controllers: { registrations: 'users/registrations' }
   devise_scope :user do
     get 'users' => 'users/registrations#index'
