c1438cd5a0f4034a8a793f15a5b916ffa03f1d95
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-09 08:47:35 +0900

    双方向の関連付けを明示する

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,9 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  has_many :active_relationships, class_name: 'Relationship', foreign_key: :following_id, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: :following_id, dependent: :destroy, inverse_of: 'following'
   has_many :followings, through: :active_relationships, source: :follower
 
-  has_many :passive_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy, inverse_of: 'follower'
   has_many :followers, through: :passive_relationships, source: :following
-
 end
