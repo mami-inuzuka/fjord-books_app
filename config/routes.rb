beef3f13b229f929fc72089a4a72ae5f4a42df14
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-08 21:35:25 +0900

    フォロー一覧とフォロワー一覧を表示するためのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: %i(index show)
   resources :users do
     resource :relationships, only: %i(create destroy)
+    get :followings
+    get :followers
   end
 end
