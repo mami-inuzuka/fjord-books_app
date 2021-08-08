a0bcc405be427adf04705a71af2033a5a21579b1
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-08 13:49:29 +0900

    relationshipsテーブルのカラム名を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_121802) do
+ActiveRecord::Schema.define(version: 2021_08_08_043924) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,8 +50,8 @@ ActiveRecord::Schema.define(version: 2021_08_07_121802) do
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.integer "following_user_id"
-    t.integer "followed_user_id"
+    t.integer "following_id"
+    t.integer "follower_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
