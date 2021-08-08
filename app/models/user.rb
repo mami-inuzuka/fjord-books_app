96cc8825f202f91fd34cd3cdce32ef179991ed59
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-08 20:39:11 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: :following_id
+  has_many :followers, through: :active_relationships, source: :Follower
+
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: :follower_id
+  has_many :followings, through: :passive_relationships, source: :Following
+
 end
