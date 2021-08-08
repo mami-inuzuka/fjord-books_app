53d784e846ed79ea11a774c76ab80609b319aa2e
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-08 20:52:48 +0900

    followingsとfollowersのアソシエーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   has_many :active_relationships, class_name: 'Relationship', foreign_key: :following_id
-  has_many :followers, through: :active_relationships, source: :Follower
+  has_many :followings, through: :active_relationships, source: :follower
 
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: :follower_id
-  has_many :followings, through: :passive_relationships, source: :Following
+  has_many :followers, through: :passive_relationships, source: :following
 
 end
