d36a9728db2f77ae2b96c2f4c8c79a7c91282f93
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-09 08:36:23 +0900

    following_idとfollower_idの組み合わせがユニークになるようにモデルとDBにバリデーションを設定する

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,5 @@
 class Relationship < ApplicationRecord
   belongs_to :following, class_name: 'User'
   belongs_to :follower, class_name: 'User'
+  validates :following_id, uniqueness: { scope: :follower_id }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_08_043924) do
+ActiveRecord::Schema.define(version: 2021_08_08_233017) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_08_08_043924) do
     t.integer "follower_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["following_id", "follower_id"], name: "index_relationships_on_following_id_and_follower_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
