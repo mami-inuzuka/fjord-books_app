6906b8c3b917a7e01acfa7fe221897b078708467
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-11 12:11:55 +0900

    ユーザー情報の編集はdeviseを利用する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  def edit
-    @user = current_user
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'books#index'
   devise_for :users
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show]
   resources :books
 end
