518d35c70d03e0e583b8f2b6fa56c831efb07924
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-18 19:50:46 +0900

    fixtureのデータを使用する

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,15 +4,15 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   test '#editable?' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    report = me.reports.build(title: 'example', content: 'password')
+    me = users(:me)
+    report = me.reports.build(title: 'example', content: 'example')
     report.save
     assert report.editable?(me)
   end
 
   test '#created_on' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    report = me.reports.build(title: 'example', content: 'password')
+    me = users(:me)
+    report = me.reports.build(title: 'example', content: 'example')
     report.created_at = '2021-08-18 00:00:00'
     report.save
     assert_equal '2021/08/18', I18n.l(report.created_on)
