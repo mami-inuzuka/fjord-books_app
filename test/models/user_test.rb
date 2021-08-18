090166d40e80b402453ff9aabe6fa3b63d24b335
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-18 18:13:51 +0900

    Userモデルのテストを作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,44 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#following?' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+    assert_not me.following?(she)
+    me.follow(she)
+    assert me.following?(she)
+  end
+
+  test '#followed_by?' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+    assert_not me.followed_by?(she)
+    she.follow(me)
+    assert me.followed_by?(she)
+  end
+
+  test '#follow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+
+    assert_not me.following?(she)
+    me.follow(she)
+    assert me.following?(she)
+  end
+
+  test '#unfollow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+    me.follow(she)
+    assert me.following?(she)
+    me.unfollow(she)
+    assert_not me.following?(she)
+  end
+
+  test '#name_or_email' do
+    user = User.new(email: 'hoge@example.com', name: '')
+    assert_equal 'hoge@example.com', user.name_or_email
+    user.name = 'Alice'
+    assert_equal 'Alice', user.name_or_email
+  end
 end
