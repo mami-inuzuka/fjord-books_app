74b028e38236dcb0ce6934d914d6c941267cef0d
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-18 18:19:12 +0900

    Reportモデルのテストを作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    report = me.reports.build(title: 'example', content: 'password')
+    report.save
+    assert report.editable?(me)
+  end
+
+  test '#created_on' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    report = me.reports.build(title: 'example', content: 'password')
+    report.save
+    assert_equal report.created_at.to_date, report.created_on
+  end
 end
