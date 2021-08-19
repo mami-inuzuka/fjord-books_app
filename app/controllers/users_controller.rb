1909a2dc82ef75ebbaadfb7f789f1a125ce564ba
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-19 17:02:20 +0900

    ユーザーインスタンスの取得を共通処理で記述する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show followings followers]
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
-  def followings
-    @user = User.find(params[:user_id])
-  end
+  def followings; end
 
-  def followers
-    @user = User.find(params[:user_id])
+  def followers; end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id] || params[:id])
   end
 end
