5ee7ca7d99943926f7bdac206f51f6ad6b3b7a37
mami-inuzuka <inuzuka.mami@gmail.com>
2021-08-19 16:51:43 +0900

    レコードの保存に失敗したとき例外を発生させるようにする

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,13 +3,13 @@
 class RelationshipsController < ApplicationController
   def create
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
-    follow.save
+    follow.save!
     redirect_to user_path(params[:user_id])
   end
 
   def destroy
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
-    follow.destroy
+    follow.destroy!
     redirect_to user_path(params[:user_id])
   end
 end
